69b7296fe91ccb77426aed9ffd62c84bdec888a6
Unknown <jeremy@dubchuck.com>
2018-09-10 14:39:07 -0700

    Note on local dev and image cp

--- camera.py
+++ camera.py
@@ -3,9 +3,12 @@ from subprocess import call
 
 import platform
 
+# If distribution is debian than we assume that the python script is running on the pi
+# and that a camera module is present.  If the distribution is not debian
+# then we assume local development is occurring and to simulate a camera capture
+# by copying an image file similar to how a camera capture occurs
 DIST = platform.dist()[0]
 DEBIAN_DIST = 'debian'
-
 LOCAL_CP_COMMAND = 'cp ./image_for_development.jpeg {}'
 
 
