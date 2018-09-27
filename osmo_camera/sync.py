a1bd770f1ddb89cc8af5ab18b13a814465ba6be8
Unknown <jeremy@dubchuck.com>
2018-09-27 11:15:16 -0700

    Updated sync using multiple processing

---
+++ osmo_camera/sync.py
@@ -0,0 +1,81 @@
+'''DOCSTRING:'''
+
+from multiprocessing import Process
+from subprocess import call
+
+# global
+SYNC_PROCESSES = dict()
+
+
+def sync_with_s3(directory='./output'):
+    '''sync a directory with s3://camera-sensor-experiments using an external
+       shell command.
+     Args:
+        directory: directory to sync
+     Returns:
+        None
+    '''
+
+    # Using CLI vs boto: https://github.com/boto/boto3/issues/358
+    # It looks like sync is not a supported function of the python boto library
+    # Work around is to use cli sync for now (requires aws cli to be installed)
+    print("Performing sync of output (experiments) folder")
+
+    # This argument pattern issues a uni-directional sync to S3 bucket
+    # https://docs.aws.amazon.com/cli/latest/reference/s3/sync.html
+    comm = 'aws s3 sync {} s3://camera-sensor-experiments'.format(directory)
+    call([comm], shell=True)
+
+
+def sync_for_directory_is_alive(directory_key):
+    '''Checks if a sync process is currently being run for the directory passed in
+     Args:
+        directory_key: key to check within the SYNC_PROCESSES dictionary
+     Returns:
+        Boolean: Used to check if a directory is currently syncing
+    '''
+    if directory_key in SYNC_PROCESSES and SYNC_PROCESSES[directory_key].is_alive():
+        return True
+
+    return False
+
+
+def end_syncing_processes():
+    '''Stop all processes currently syncing.  Intended to be used
+       if experimental image capture has finished and a final
+       sync should be initiated.
+     Args:
+        None
+     Returns:
+        None
+    '''
+
+    for directory_key in SYNC_PROCESSES:
+        process = SYNC_PROCESSES[directory_key]
+        process.stop()
+        SYNC_PROCESSES[directory_key] = None
+
+
+# TODO: Process limiting?  Fast-follow ticket using a queue?'''
+# TODO: Test in real life: Will process management algorithm cause inconsitencies with syncing'''
+
+def sync_directory_in_separate_process(directory, final_sync=False):
+    '''Creates process for syncing a directory
+     Args:
+        directory: directory to sync
+        join_process: Should the newly invoked process be completed before
+        returning from the function (async/sync)
+     Returns:
+        None.  To different code paths
+        Fail silently if directory is currently syncing
+    '''
+    if sync_for_directory_is_alive(directory):
+        return
+
+    sync_process = Process(target=sync_with_s3, args=(directory,))
+    sync_process.start()
+    SYNC_PROCESSES[directory] = sync_process
+
+    if final_sync:
+        end_syncing_processes()
+        sync_process.join()
---
+++ osmo_camera/sync_test.py
@@ -0,0 +1,17 @@
+# from sync import sync_with_s3, sync_for_directory_is_alive, end_syncing_processes, sync_directory_in_separate_process
+
+
+def sync_with_s3_test():
+    '''TODO:'''
+
+
+def sync_for_directory_is_alive_test():
+    '''TODO:'''
+
+
+def end_syncing_processes_test():
+    '''TODO:'''
+
+
+def sync_directory_in_separate_process_test():
+    '''TODO:'''
