cca50996efee89f729db78bae90cca788ed17c4b
Unknown <jeremy@dubchuck.com>
2018-10-09 11:59:58 -0700

    Add comment and ref to ticket for usage of recursion or single folder

--- osmo_camera/raw/convert.py
+++ osmo_camera/raw/convert.py
@@ -32,6 +32,9 @@ def _folder_to_dng(raspiraw_location, raw_images_dir):
 
 def _folder_to_dng_recursive(raspiraw_location, raw_images_dir):
     # Convert .jpegs in folder recursively
+    # NOTE: Currently recursive conversion is not our intended method of extracting
+    # raw data to a DNG file.  (This was a one off request by Jacob for support)
+    # TODO: Determine which to use - https://app.asana.com/0/823265982730077/858190003215284
     for root, _, files in os.walk(raw_images_dir):
         for file in files:
             if os.path.splitext(file)[1].lower() in ('.jpg', '.jpeg'):
