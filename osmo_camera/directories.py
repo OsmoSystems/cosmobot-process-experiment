6e40c152b831a1583e2c94f2e6a9cb28f7f9bf9e
Unknown <jeremy@dubchuck.com>
2018-10-17 09:53:05 -0700

    Create directory with a full path

--- osmo_camera/directories.py
+++ osmo_camera/directories.py
@@ -23,9 +23,13 @@ def get_files_with_extension(directory, extension):
 
 
 def create_output_directory(base_directory, new_directory_name):
-    '''Create a new directory if it does not exist'''
+    '''Create a new directory with a base path if it does not exist'''
     new_directory_path = os.path.join(base_directory, new_directory_name)
+    return create_directory(new_directory_path)
 
+
+def create_directory(new_directory_path):
+    '''Create a new directory if it does not exist'''
     if not os.path.exists(new_directory_path):
         print(f'creating folder: {new_directory_path}')
         os.mkdir(new_directory_path)
