5c4bb4328b36582e796f1ed5781ebe5dee29745c
Jaime <jaime@osmobot.com>
2018-10-17 09:18:43 -0700

    Tweak test

--- osmo_camera/file_structure_test.py
+++ osmo_camera/file_structure_test.py
@@ -71,7 +71,7 @@ def test_get_files_with_extension(mock_listdir, mock_path_join, name, directory_
 
 class TestIsoDatetimeForFilename:
     def test_returns_iso_ish_string(self):
-        actual = module.iso_datetime_for_filename(datetime(2018, 1, 1, 12, 1, 1))
-        expected = '2018-01-01--12-01-01'
+        actual = module.iso_datetime_for_filename(datetime(2018, 1, 2, 13, 14, 15))
+        expected = '2018-01-02--13-14-15'
 
         assert actual == expected
