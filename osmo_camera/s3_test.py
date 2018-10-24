a3374c8778a904cffbf463221f0f3676806770ab
Jaime <jaime@osmobot.com>
2018-10-23 17:03:33 -0700

    Remove code related to run_experiment, update setup.py

--- osmo_camera/s3_test.py
+++ osmo_camera/s3_test.py
@@ -61,14 +61,3 @@ class TestSyncFromS3:
         module.sync_from_s3('experiment_directory_name', local_sync_dir)
 
         mock_path_join.assert_called_with(expected_sync_dir, 'experiment_directory_name')
-
-
-class TestSyncToS3:
-    def test_syncs_to_subdirectory_in_s3_bucket(self, mocker, mock_check_call):
-        mock_path_basename = mocker.patch('os.path.basename')
-        mock_path_basename.return_value = 'experiment_name'
-
-        module.sync_to_s3(local_sync_dir='/output_dir/experiment_name')
-
-        expected_command = f'aws s3 sync /output_dir/experiment_name s3://camera-sensor-experiments/experiment_name'
-        mock_check_call.assert_called_with(expected_command, shell=True)
--- setup.py
+++ setup.py
@@ -6,12 +6,10 @@ setup(
     version='0.0.1',
     author='Osmo Systems',
     author_email='dev@osmobot.com',
-    description='Prototype code for an osmobot camera sensor',
-    url='https://www.github.com/osmosystems/camera-sensor-prototype',
+    description='Code for processing image-collection experiments',
+    url='https://www.github.com/osmosystems/cosmobot-process-experiment.git',
     packages=find_packages(),
-    entry_points={
-        'console_scripts': ['run_experiment = osmo_camera.experiment:run_experiment']
-    },
+    entry_points={},
     install_requires=[
         'boto',
         'ipywidgets',
