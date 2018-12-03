24446229d7f959421f6e7a325c1b959ec5dd7bf7
Unknown <jeremy@dubchuck.com>
2018-12-03 14:30:52 -0800

    Update average to expect rgb data

--- osmo_camera/rgb/average.py
+++ osmo_camera/rgb/average.py
@@ -1,6 +1,8 @@
 from . import crop
 
+IMAGE_AXIS = (0, 1)
+
 
 def spatial_average_of_roi(input_rgb, ROI):
     image_crop = crop.crop_image(input_rgb, ROI)
-    return image_crop.mean()
+    return image_crop.mean(axis=IMAGE_AXIS)
--- osmo_camera/rgb/average_test.py
+++ osmo_camera/rgb/average_test.py
@@ -3,14 +3,16 @@ import numpy as np
 import osmo_camera.rgb.average as module
 
 
-def test_can_average_roi_spatially():
+def test_can_average_ROI_spatially():
     input_rgb = np.array([
-        [1, 2, 3, 4],
-        [5, 6, 7, 8],
-        [9, 10, 11, 12]
+        [[0, 0, 1], [0, 0, 1], [0, 0, 1]],
+        [[0, 0, 2], [0, 0, 2], [0, 0, 3]],
+        [[0, 0, 3], [0, 0, 4], [0, 0, 5]],
     ])
 
-    roi_to_spatially_average = (1, 1, 2, 2)
+    ROI_to_spatially_average = (1, 1, 2, 2)
 
-    expected = 8.5
-    assert module.spatial_average_of_roi(input_rgb, roi_to_spatially_average) == expected
+    expected = 3.5
+    blue_channel = 2
+
+    assert module.spatial_average_of_roi(input_rgb, ROI_to_spatially_average)[blue_channel] == expected
