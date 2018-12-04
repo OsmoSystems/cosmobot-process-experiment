e58b2f4b36909ea36709041fd452a72d3a77476a
Unknown <jeremy@dubchuck.com>
2018-12-04 06:12:57 -0800

    ROI -> ROI_definition

--- osmo_camera/rgb/average.py
+++ osmo_camera/rgb/average.py
@@ -3,6 +3,6 @@ from . import crop
 IMAGE_AXIS = (0, 1)
 
 
-def spatial_average_of_roi(input_rgb, ROI):
-    image_crop = crop.crop_image(input_rgb, ROI)
+def spatial_average_of_roi(input_rgb, ROI_definition):
+    image_crop = crop.crop_image(input_rgb, ROI_definition)
     return image_crop.mean(axis=IMAGE_AXIS)
