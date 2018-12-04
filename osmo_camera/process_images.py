d40a3662ed8204f27b97544e68c25faf0c8c53c5
Unknown <jeremy@dubchuck.com>
2018-12-04 14:44:55 -0800

    Naming, dict for correct images (for comprehensions), correction test

--- osmo_camera/process_images.py
+++ osmo_camera/process_images.py
@@ -35,7 +35,7 @@ ROI_STATISTIC_CALCULATORS: Dict[str, Callable] = {
 }
 
 
-def _process_ROI(ROI):
+def _get_ROI_statistics(ROI):
     channel_stats = {
         stat_name: stat_calculator(ROI)
         for stat_name, stat_calculator in ROI_STATISTIC_CALCULATORS.items()
@@ -69,7 +69,7 @@ def save_ROI_crops(ROI_crops_dir, dng_image_path, rgb_ROIs_by_name):
         rgb.save.as_file(rgb_ROI, ROI_crop_path)
 
 
-def ROI_analysis(rgb_image, dng_image_path, ROI_definitions, ROI_crops_dir=None):
+def process_ROIs(rgb_image, dng_image_path, ROI_definitions, ROI_crops_dir=None):
     ''' Process all the ROIs in a single .DNG image into summary statistics
 
     For each ROI:
@@ -102,63 +102,53 @@ def ROI_analysis(rgb_image, dng_image_path, ROI_definitions, ROI_crops_dir=None)
             'image': os.path.basename(dng_image_path),
             'ROI': ROI_name,
             'ROI definition': ROI_definitions[ROI_name],
-            **_process_ROI(ROI)
+            **_get_ROI_statistics(ROI)
         }
         for ROI_name, ROI in ROIs.items()
     ]
 
 
 def correct_images(
-    dng_image_paths,
-    ROI_definitions=[],
-    ROI_definition_for_intensity_correction=(0,0,0,0),
+    original_rgb_by_filepath,
+    ROI_definition_for_intensity_correction,
 ):
-    ''' Process all images from an experiment:
+    ''' Correct all images from an experiment:
         1. Apply dark frame correction
         2. Apply flat field correction
         3. Apply intensity correction
 
     Args:
-        dng_image_paths: list of file paths for dngs to be processed into a "blue" value
-        ROI_definitions: list of ROIs that are averaged for
+        original_rgb_by_filepath: { image_path : image_rgb }
         ROI_definition_for_intensity_correction: ROI to average and use to correct intensity on `ROI_definitions`
      Returns:
         A dictionary of intensity corrected rgb images that is keyed by dng file path
     '''
 
-    # Image corrections and blueness calculation
-    dark_frame_corrected_rgb_by_filepath = dict()
-    flat_field_corrected_rgb_by_filepath = dict()
-    intensity_corrected_rgb_by_filepath = dict()
-
+    print('--------Correcting Images--------')
     print('1. Apply dark frame correction')
-    # open all images and perform dark frame correction
-    for image_path in dng_image_paths:
-        image_rgb = dng.open.as_rgb(image_path)
-        dark_frame_rgb = np.zeros(image_rgb.shape)
-        dark_frame_corrected_rgb_by_filepath[image_path] = dark_frame.apply_dark_frame_correction(
-            image_rgb,
-            dark_frame_rgb
-        )
+    dark_frame_corrected_rgb_by_filepath = {
+        image_path: dark_frame.apply_dark_frame_correction(image_rgb, np.zeros(image_rgb.shape))
+        for image_path, image_rgb in original_rgb_by_filepath.items()
+    }
 
     print('2. Apply flat field correction')
-    for image_path in dark_frame_corrected_rgb_by_filepath:
-        dark_frame_corrected_rgb = np.array(dark_frame_corrected_rgb_by_filepath[image_path])
-        dark_frame_rgb = np.zeros(dark_frame_corrected_rgb.shape)
-        flat_field_rgb = np.zeros(dark_frame_corrected_rgb.shape)
-
-        flat_field_corrected_rgb_by_filepath[image_path] = flat_field.apply_flat_field_correction(
+    flat_field_corrected_rgb_by_filepath = {
+        image_path: flat_field.apply_flat_field_correction(
             dark_frame_corrected_rgb,
-            dark_frame_rgb,
-            flat_field_rgb
+            dark_frame_rgb=np.zeros(dark_frame_corrected_rgb.shape),
+            flat_field_rgb=np.ones(dark_frame_corrected_rgb.shape)
         )
+        for image_path, dark_frame_corrected_rgb in dark_frame_corrected_rgb_by_filepath.items()
+    }
 
     print('3. Apply intensity correction')
-    for image_path in flat_field_corrected_rgb_by_filepath:
-        intensity_corrected_rgb_by_filepath[image_path] = intensity.apply_intensity_correction(
-            flat_field_corrected_rgb_by_filepath[image_path],
+    intensity_corrected_rgb_by_filepath = {
+        image_path: intensity.apply_intensity_correction(
+            flat_field_corrected_rgb,
             ROI_definition_for_intensity_correction
         )
+        for image_path, flat_field_corrected_rgb in flat_field_corrected_rgb_by_filepath.items()
+    }
 
     return intensity_corrected_rgb_by_filepath
 
@@ -175,6 +165,7 @@ def process_images(dng_images_dir, ROI_definitions, save_ROIs=False):
     Returns:
         An pandas DataFrame in which each row contains summary statistics for a single ROI in a single image
     '''
+
     dng_image_paths = get_files_with_extension(dng_images_dir, '.dng')
 
     # If ROI crops should be saved, create a directory for them
@@ -183,22 +174,26 @@ def process_images(dng_images_dir, ROI_definitions, save_ROIs=False):
         ROI_crops_dir = create_output_directory(dng_images_dir, 'ROI crops')
         print('ROI crops saved in:', ROI_crops_dir)
 
-    dummy_intensity_correction_ROI = (0,0,0,0)
+    dummy_intensity_correction_ROI = (0, 0, 0, 0)
 
-    intensity_corrected_rgb_images = correct_images(
-        dng_image_paths,
-        ROI_definitions,
+    original_rgb_by_filepath = {
+        image_path: dng.open.as_rgb(image_path)
+        for image_path in dng_image_paths
+    }
+
+    corrected_rgb_images = correct_images(
+        original_rgb_by_filepath,
         ROI_definition_for_intensity_correction=dummy_intensity_correction_ROI
     )
 
-    processed_images = [
-        ROI_analysis(rgb_image, dng_image_path, ROI_definitions, ROI_crops_dir)
-        for dng_image_path, rgb_image in intensity_corrected_rgb_images.items()
+    processed_ROIs = [
+        process_ROIs(rgb_image, dng_image_path, ROI_definitions, ROI_crops_dir)
+        for dng_image_path, rgb_image in corrected_rgb_images.items()
     ]
 
     summary_statistics = pd.DataFrame(
         # Flatten all ROI summaries for all images into a single 1D list
-        list(chain.from_iterable(processed_images))
+        list(chain.from_iterable(processed_ROIs))
     ).sort_values('timestamp').reset_index(drop=True)
 
     return summary_statistics
