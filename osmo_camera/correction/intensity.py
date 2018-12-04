c92efb78c31f4cd33856970a86f91c1012fc7a6a
Unknown <jeremy@dubchuck.com>
2018-12-04 14:01:50 -0800

    Remove spatial averaging for intensity correction

--- osmo_camera/correction/intensity.py
+++ osmo_camera/correction/intensity.py
@@ -1,9 +1,2 @@
-from osmo_camera import rgb
-
 def apply_intensity_correction(input_rgb, ROI_definition_for_intensity_correction):
-    intensity_correction_roi_spatial_average = rgb.average.spatial_average_of_roi(  # pylint: disable=W0612
-        input_rgb,
-        ROI_definition_for_intensity_correction
-    )
-
     return input_rgb
