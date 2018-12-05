5fac1042f50c576aaafa3ff730c9eefd293b38fb
Jaime <jaime@osmobot.com>
2018-12-04 17:26:21 -0800

    Add ticketed TODOs for implementing pipeline steps & rename tests

--- osmo_camera/correction/dark_frame_test.py
+++ osmo_camera/correction/dark_frame_test.py
@@ -5,11 +5,13 @@ import numpy as np
 import osmo_camera.correction.dark_frame as module
 
 
-def test_correction_with_dark_frame():
+def test_apply_dark_frame_correction():
     input_rgb = np.array([
         [11, 12, 13, 14],
         [15, 16, 17, 18],
         [19, 20, 21, 22]
     ])
 
-    np.testing.assert_array_equal(module.apply_dark_frame_correction(input_rgb, sentinel.dark_frame_rgb), input_rgb)
+    actual = module.apply_dark_frame_correction(input_rgb, sentinel.dark_frame_rgb)
+
+    np.testing.assert_array_equal(actual, input_rgb)
