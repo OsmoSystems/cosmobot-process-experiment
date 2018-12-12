e376f3de9785c1f4242f9515f7908634b8e3634e
Unknown <jeremy@dubchuck.com>
2018-12-12 07:30:03 -0800

    a nice little docstring

--- osmo_camera/correction/dark_frame.py
+++ osmo_camera/correction/dark_frame.py
@@ -1,11 +1,18 @@
-# Details for dark frame correction taken from
-# https://docs.google.com/document/d/1xIgZxrC1qYUnwEGWt8yXnvWluEj51jpyIqArMJlNhrs/edit#
 EXPOSURE_SLOPE = 0.069759
 DARK_OFFSET = 63.895291
 RAW_BIT_DEPTH = 2**10
 
 
 def _calculate_dark_signal_in_dnr(exposure_seconds):
+    ''' Calculate dark signal based on final recommendation:
+        https://docs.google.com/document/d/1xIgZxrC1qYUnwEGWt8yXnvWluEj51jpyIqArMJlNhrs/edit#
+
+    Args:
+        exposure_seconds: number of seconds for exposure retrieved from exif_tags in image
+
+    Returns:
+        A value representing the dark signal that is normalized from DN (0-1024) to DNR (0-1)
+    '''
     return ((EXPOSURE_SLOPE * exposure_seconds) + DARK_OFFSET) / RAW_BIT_DEPTH
 
 
