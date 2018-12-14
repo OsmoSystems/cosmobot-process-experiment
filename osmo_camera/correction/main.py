5dc89bf9cee2c7a7b578ce992e2c3747302e8e6a
Unknown <jeremy@dubchuck.com>
2018-12-14 06:38:11 -0800

    remove summary image implementation

--- osmo_camera/correction/main.py
+++ osmo_camera/correction/main.py
@@ -4,31 +4,14 @@ from osmo_camera import rgb, file_structure
 
 def save_rgb_images_by_filepath_with_suffix(
     rgb_images_by_filepath,
-    filepath_suffix,
-    save_all_images
+    filepath_suffix
 ):
-    images_to_save = rgb_images_by_filepath if save_all_images else first_middle_last_rgb_images(rgb_images_by_filepath)
-
-    for image_path, image_rgb in images_to_save.items():
+    for image_path, image_rgb in rgb_images_by_filepath.items():
         if not file_structure.file_exists_and_size_is_not_zero(image_path):
             rgb.save.as_file(
                 image_rgb,
                 file_structure.append_suffix_to_filepath_before_extension(image_path, filepath_suffix)
             )
-
-
-def first_middle_last_rgb_images(rgb_images_by_filepath):
-    sorted_rgb_image_keys = sorted(rgb_images_by_filepath.keys())  # Assumes images are prefixed with iso-ish datetimes
-    keys_to_use = [
-        sorted_rgb_image_keys[0],
-        sorted_rgb_image_keys[int(len(sorted_rgb_image_keys) / 2)],
-        sorted_rgb_image_keys[-1]
-    ]
-
-    return {
-        image_path: rgb_images_by_filepath[image_path]
-        for image_path in keys_to_use
-    }
 
 
 def correct_images(
@@ -56,24 +39,18 @@ def correct_images(
         original_rgb_by_filepath
     )
 
-    print('Saving dark frame corrected images')
-    save_rgb_images_by_filepath_with_suffix(
-        dark_frame_corrected_rgb_by_filepath,
-        "_dark_adj",
-        save_dark_frame_corrected_images
-    )
+    if save_dark_frame_corrected_images:
+        print('Saving dark frame corrected images')
+        save_rgb_images_by_filepath_with_suffix(dark_frame_corrected_rgb_by_filepath, "_dark_adj")
 
     print('2. Apply flat field correction, but not really')
     flat_field_corrected_rgb_by_filepath = flat_field.apply_flat_field_correction_to_rgb_images(
         dark_frame_corrected_rgb_by_filepath
     )
 
-    print('Saving flat field corrected images')
-    save_rgb_images_by_filepath_with_suffix(
-        flat_field_corrected_rgb_by_filepath,
-        "_dark_flat_adj",
-        save_flat_field_corrected_images
-    )
+    if save_flat_field_corrected_images:
+        print('Saving flat field corrected images')
+        save_rgb_images_by_filepath_with_suffix(flat_field_corrected_rgb_by_filepath, "_dark_flat_adj")
 
     print('3. Apply intensity correction, but not really')
     intensity_corrected_rgb_by_filepath = intensity.apply_intensity_correction_to_rgb_images(
@@ -81,12 +58,8 @@ def correct_images(
         ROI_definition_for_intensity_correction
     )
 
-    # TODO: save with each correction or batch save after all corrections are applied?
-    print('Saving intensity corrected images')
-    save_rgb_images_by_filepath_with_suffix(
-        intensity_corrected_rgb_by_filepath,
-        "_dark_flat_intensity_adj",
-        save_intensity_corrected_images
-    )
+    if save_intensity_corrected_images:
+        print('Saving intensity corrected images')
+        save_rgb_images_by_filepath_with_suffix(intensity_corrected_rgb_by_filepath, "_dark_flat_intensity_adj")
 
     return intensity_corrected_rgb_by_filepath
