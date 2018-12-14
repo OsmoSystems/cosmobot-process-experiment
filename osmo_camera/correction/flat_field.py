2694a7b99d469cd587ff3b6e9fe22f0e9c61913b
Unknown <jeremy@dubchuck.com>
2018-12-14 06:28:47 -0800

    missed a few

--- osmo_camera/correction/flat_field.py
+++ osmo_camera/correction/flat_field.py
@@ -10,8 +10,8 @@ def apply_flat_field_correction_to_rgb_images(rgbs_by_filepath):
     flat_field_corrected_rgbs_by_filepath = {
         image_path: _apply_flat_field_correction(
             image_rgb,
-            dark_frame_rgb=np.zeros(dark_frame_corrected_rgb.shape),
-            flat_field_rgb=np.ones(dark_frame_corrected_rgb.shape)
+            dark_frame_rgb=np.zeros(image_rgb.shape),
+            flat_field_rgb=np.ones(image_rgb.shape)
         )
         for image_path, image_rgb in rgbs_by_filepath.items()
     }
