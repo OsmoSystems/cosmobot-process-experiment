6808b559a0c8ec1c1d6e6e8cdd73d590809ebbc7
thatneat <thatneat@gmail.com>
2019-01-02 15:10:55 -0800

    CR feedback: standardize and simplify

--- osmo_camera/correction/flat_field.py
+++ osmo_camera/correction/flat_field.py
@@ -1,5 +1,3 @@
-import numpy as np
-import pandas as pd
 
 
 def _apply_flat_field_correction(dark_frame_corrected_rgb, dark_frame_rgb, flat_field_rgb):
@@ -8,13 +6,8 @@ def _apply_flat_field_correction(dark_frame_corrected_rgb, dark_frame_rgb, flat_
 
 
 def apply_flat_field_correction_to_rgb_images(rgbs_by_filepath):
-    flat_field_corrected_rgbs_by_filepath = pd.Series({
-        image_path: _apply_flat_field_correction(
-            image_rgb,
-            dark_frame_rgb=np.zeros(image_rgb.shape),
-            flat_field_rgb=np.ones(image_rgb.shape)
-        )
-        for image_path, image_rgb in rgbs_by_filepath.items()
-    })
-
-    return flat_field_corrected_rgbs_by_filepath
+    return rgbs_by_filepath.apply(
+        _apply_flat_field_correction,
+        dark_frame_rgb=None,
+        flat_field_rgb=None
+    )
