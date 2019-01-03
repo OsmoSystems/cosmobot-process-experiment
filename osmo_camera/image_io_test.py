e26d788608e5e6dc09db064ef0eb776dd3969b4a
Unknown <jeremy@dubchuck.com>
2019-01-03 12:51:47 -0800

    Cycle the build

--- osmo_camera/image_io_test.py
+++ osmo_camera/image_io_test.py
@@ -19,7 +19,7 @@ from osmo_camera import tiff as module
     ])),
 ])
 def test_rgb_image_saved_to_tiff_file_and_loaded_retains_data(tmp_path, name, test_rgb_image):
-    absolute_tolerance = 1.862645e-9  # 1/2^29 (quantization error when saving to and reading from an rgb image)
+    absolute_tolerance = 1.862645e-9  # 1 / 2^29 (quantization error when saving to and reading from an rgb image)
 
     # tmp_path provides an object of type PosixPath, tifffile expects a file path as a string
     tmp_filepath = str(tmp_path / 'test.tiff')
