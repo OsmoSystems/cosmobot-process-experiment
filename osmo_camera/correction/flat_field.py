22edd89a913fb5ea2399860e0a0b6ba7e2e474b9
Unknown <jeremy@dubchuck.com>
2019-01-03 14:36:48 -0800

    Remove duplicate, comment

--- osmo_camera/correction/flat_field.py
+++ osmo_camera/correction/flat_field.py
@@ -12,16 +12,3 @@ def apply_flat_field_correction_to_rgb_images(rgbs_by_filepath):
         dark_frame_rgb=None,
         flat_field_rgb=None
     )
-
-
-def generate_flat_field(rgbs_by_filepath):
-    # Subtract dark frame (using exposure of image) for all images
-    dark_frame_adjusted_rgb_images = dark_frame.apply_dark_frame_correction_to_rgb_images(rgbs_by_filepath)
-
-    # Average (image) of all dark frame adjusted images
-    flat_field_mean_of_image_stack = dark_frame_adjusted_rgb_images.mean(axis=0)
-
-    # Average value of averaged image
-    flat_field_mean = flat_field_mean_of_image_stack.mean(axis=(0, 1))
-
-    return flat_field_mean / flat_field_mean_of_image_stack
--- osmo_camera/generate_flat_field.py
+++ osmo_camera/generate_flat_field.py
@@ -18,7 +18,7 @@ def from_rgb_images(rgbs_by_filepath):
     # Average (image) of all dark frame adjusted images
     flat_field_mean_of_image_stack = dark_frame_adjusted_rgb_images.mean(axis=0)
 
-    # Average value of averaged image
+    # Average RGB value of averaged image
     flat_field_mean = flat_field_mean_of_image_stack.mean(axis=(0, 1))
 
     return flat_field_mean / flat_field_mean_of_image_stack
