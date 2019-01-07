add49db8ba6223dcd985eca7ca33e0acb76b3861
thatneat <thatneat@users.noreply.github.com>
2019-01-07 11:28:52 -0800

    warp --> wrap
    Co-Authored-By: jaimemarijke <jaimemarijke@gmail.com>

--- osmo_camera/tiff/save.py
+++ osmo_camera/tiff/save.py
@@ -33,7 +33,7 @@ def as_tiff(rgb_image, output_path):
     '''
     scaled_rgb_image = rgb_image * DNR_TO_TIFF_FACTOR
 
-    # Guard that the image will fit in 32-bits. Otherwise, coercing to np.int32 will cause it to warparound
+    # Guard that the image will fit in 32 bits. Otherwise, coercing to np.int32 will cause it to wrap around
     _guard_image_fits_in_32_bits(scaled_rgb_image)
     rgb_image_as_uint_array = scaled_rgb_image.astype(np.int32)
     tifffile.imsave(output_path, rgb_image_as_uint_array, compress=1)
