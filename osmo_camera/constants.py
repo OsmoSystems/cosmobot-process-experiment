fe97fe078570765d993e148e34a82360c0d2f5ce
Jaime <jaime@osmobot.com>
2019-02-04 15:16:25 -0800

    Bump tiff padding bits from 2 to 6

--- osmo_camera/constants.py
+++ osmo_camera/constants.py
@@ -2,6 +2,6 @@ RAW_BIT_DEPTH = 2**10  # used for normalizing DN to DNR
 COLOR_CHANNEL_INDICES = [0, 1, 2]
 
 # https://docs.google.com/document/d/1yS73HuP4B1_bWbD2lI5cMBV5K6DHEkGePcHr8gBs-EE/edit#
-# TL;DR We save signed data (requires 1 bit) and 2 bits of “padding” around the upper end of the range.
-# 32 - 1 - 2 = 29.
-DNR_TO_TIFF_FACTOR = 2**29
+# TL;DR We save signed data (requires 1 bit) and 6 bits of “padding” around the upper end of the range.
+# 32 - 1 - 6 = 25.
+DNR_TO_TIFF_FACTOR = 2**25
--- osmo_camera/tiff/image_io_test.py
+++ osmo_camera/tiff/image_io_test.py
@@ -5,17 +5,13 @@ from osmo_camera import tiff as module
 
 
 @pytest.mark.parametrize("name, test_rgb_image", [
-    ('0 to 1 DNR', np.array([
-        [[0.0, 0.0, 0.0], [0.499999, 0.499999, 0.499999]],
-        [[0.699999, 0.699999, 0.699999], [0.999999, 0.999999, 0.999999]]
+    ('Within [0, 1) DNR', np.array([
+        [[0.0, 0.0, 0.0], [0.1, 0.2, 0.3]],
+        [[0.4, 0.5, 0.6], [0.7, 0.8, 0.9]]
     ])),
-    ('-1 to +1 DNR', np.array([
-        [[-0.999999, -0.999999, -0.999999], [-0.699999, -0.699999, -0.699999]],
-        [[0.699999, 0.699999, 0.699999], [0.999999, 0.999999, 0.999999]]
-    ])),
-    ('-3 to +3 DNR', np.array([
-        [[-2.999999, -2.999999, -2.999999], [-2.699999, -2.699999, -2.699999]],
-        [[2.699999, 2.699999, 2.699999], [2.999999, 2.999999, 2.999999]]
+    ('Within padded [-4, 4) DNR', np.array([
+        [[-64, -64, -64], [-0.5, -0.5, -0.5]],
+        [[0.5, 0.5, 0.5], [63.999999, 63.999999, 63.999999]]
     ])),
 ])
 def test_rgb_image_saved_to_tiff_file_and_loaded_retains_data(tmp_path, name, test_rgb_image):
@@ -30,14 +26,19 @@ def test_rgb_image_saved_to_tiff_file_and_loaded_retains_data(tmp_path, name, te
     np.testing.assert_allclose(test_rgb_image, expected_tmp_tiff_as_rgb_image, atol=absolute_tolerance)
 
 
-def test_tiff_save_raises_if_image_out_of_range(tmp_path):
+@pytest.mark.parametrize("name, test_rgb_image", [
+    ('Below min', np.array([
+        [[0, 0, 0], [-0.5, -0.5, -0.5]],
+        [[0.5, 0.5, 0.5], [-64.01, -64.01, -64.01]]
+    ])),
+    ('Above max', np.array([
+        [[0, 0, 0], [-0.5, -0.5, -0.5]],
+        [[0.5, 0.5, 0.5], [64.01, 64.01, 64.01]]
+    ])),
+])
+def test_tiff_save_raises_if_image_out_of_range(tmp_path, name, test_rgb_image):
     # tmp_path provides an object of type PosixPath, tifffile expects a file path as a string
     tmp_filepath = str(tmp_path / 'test_tiff.tiff')
-
-    test_rgb_image = np.array([
-        [[-4.01, -4.01, -4.01], [-1, -1, -1]],
-        [[1, 1, 1], [4.01, 4.01, 4.01]]
-    ])
 
     with pytest.raises(module.save.DataTruncationError):
         module.save.as_tiff(test_rgb_image, tmp_filepath)
