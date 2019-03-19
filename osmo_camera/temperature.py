e93b7e9f0b6124c875a33372566eee5ac4a298de
Unknown <jeremy@dubchuck.com>
2019-03-19 08:30:39 -0700

    match process experiment signature

--- osmo_camera/temperature.py
+++ osmo_camera/temperature.py
@@ -4,6 +4,7 @@ import pandas as pd
 def _read_temperature_log(filepath):
     return pd.read_csv(filepath)
 
-def temperature_for_experiment(experiment_directory_path):
+def temperature_for_experiment(local_sync_directory_path, experiment_name):
+    experiment_directory_path = os.path.join(local_sync_directory_path, experiment_name)
     temperature_filepath = os.path.join(experiment_directory_path, 'temperature.csv')
     return _read_temperature_log(temperature_filepath)
