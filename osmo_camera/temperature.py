461c22153c0ac8ef2f5dd181a9f4b91f5799c65d
Unknown <jeremy@dubchuck.com>
2019-03-19 08:24:51 -0700

    read temperature csv

--- osmo_camera/temperature.py
+++ osmo_camera/temperature.py
@@ -1,4 +1,9 @@
+import os
 import pandas as pd
 
-def temperature_log(filepath):
+def _read_temperature_log(filepath):
     return pd.read_csv(filepath)
+
+def temperature_for_experiment(experiment_directory_path):
+    temperature_filepath = os.path.join(experiment_directory_path, 'temperature.csv')
+    return _read_temperature_log(temperature_filepath)
