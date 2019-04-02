69f9506b1fc11c73772f6d1bf5235dafd864335a
thatneat <thatneat@users.noreply.github.com>
2019-04-02 15:12:00 -0700

    Tiff saving tests were simultaneously too lenient and too stringent (#65)
    * Tiff saving tests were simultaneously too lenient and too stringent
    
    test fixture was too easy to save/load, and tolerance was improperly tight
    
    * simplify

--- osmo_camera/tiff/image_io_test.py
+++ osmo_camera/tiff/image_io_test.py
@@ -2,28 +2,33 @@ import pytest  # noqa: F401 (imported but unused)
 import numpy as np
 
 from osmo_camera import tiff as module
+from osmo_camera.constants import DNR_TO_TIFF_FACTOR
 
 
 @pytest.mark.parametrize("name, test_rgb_image", [
     ('Within [0, 1) DNR', np.array([
-        [[0.0, 0.0, 0.0], [0.1, 0.2, 0.3]],
+        [[0, 0, 0], [0.1, 0.2, 0.3]],
         [[0.4, 0.5, 0.6], [0.7, 0.8, 0.9]]
     ])),
-    ('Within padded [-4, 4) DNR', np.array([
+    ('Within padded [-64, 64) DNR', np.array([
         [[-64, -64, -64], [-0.5, -0.5, -0.5]],
         [[0.5, 0.5, 0.5], [63.999999, 63.999999, 63.999999]]
     ])),
+    ('Challenging high-precision fractions', np.array([
+        [[1 / 3, 2**-25, 2**-29], [np.pi, np.e, -np.pi]]
+    ])),
 ])
 def test_rgb_image_saved_to_tiff_file_and_loaded_retains_data(tmp_path, name, test_rgb_image):
-    absolute_tolerance = 1.862645e-9  # 1 / 2^29 (quantization error when saving to and reading from an rgb image)
+    absolute_tolerance = 1 / DNR_TO_TIFF_FACTOR  # quantization error when saving to and reading from an rgb image
+    assert absolute_tolerance == 2**-25  # Reminder to take another look at this test if the constant changes
 
     # tmp_path provides an object of type PosixPath, tifffile expects a file path as a string
     tmp_filepath = str(tmp_path / 'test_tiff.tiff')
     module.save.as_tiff(test_rgb_image, tmp_filepath)
 
     # read rgb_image from tmp tiff (should convert)
-    expected_tmp_tiff_as_rgb_image = module.open.as_rgb(tmp_filepath)
-    np.testing.assert_allclose(test_rgb_image, expected_tmp_tiff_as_rgb_image, atol=absolute_tolerance)
+    actual_tmp_tiff_as_rgb_image = module.open.as_rgb(tmp_filepath)
+    np.testing.assert_allclose(test_rgb_image, actual_tmp_tiff_as_rgb_image, atol=absolute_tolerance)
 
 
 @pytest.mark.parametrize("name, test_rgb_image", [
