447544679d221570fe2bd414d61e940979150c25
Jason Curtis <thatneat@gmail.com>
2019-04-02 15:10:42 -0700

    simplify

--- osmo_camera/tiff/image_io_test.py
+++ osmo_camera/tiff/image_io_test.py
@@ -20,7 +20,7 @@ from osmo_camera.constants import DNR_TO_TIFF_FACTOR
 ])
 def test_rgb_image_saved_to_tiff_file_and_loaded_retains_data(tmp_path, name, test_rgb_image):
     absolute_tolerance = 1 / DNR_TO_TIFF_FACTOR  # quantization error when saving to and reading from an rgb image
-    assert 1 / DNR_TO_TIFF_FACTOR == 2**-25  # Reminder to take another look at this test if the constant changes
+    assert absolute_tolerance == 2**-25  # Reminder to take another look at this test if the constant changes
 
     # tmp_path provides an object of type PosixPath, tifffile expects a file path as a string
     tmp_filepath = str(tmp_path / 'test_tiff.tiff')
