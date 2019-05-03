62ab51a6b8e8a1dbaeec0641d0717a106d5aee4c
Jaime <jaime@osmobot.com>
2019-05-03 15:25:15 -0700

    Generic and hardcoded temperature calibration functions (#73)
    * Generic and hardcoded temperature calibration functions
    
    * remove vestigial test defaults
    
    * CR edits: renamed module, decapitalized parameters, etc.
    
    * minor param renames and comments

---
+++ osmo_camera/calibration/temperature.py
@@ -0,0 +1,185 @@
+import numpy as np
+
+
+# ADS1x15 datasheet:
+# Adafruit 4-Channel ADC Breakouts https://drive.google.com/open?id=1VpoROFyzoGa0YAAWzQccNExRHWgEfLj2
+ADS1115_MAX_VOLTAGE_AT_GAIN_1 = 4.096
+ADS1115_BIT_DEPTH = 16
+RASPBERRY_PI_VOLTAGE_3_3 = 3.3
+
+
+def _celcius_to_kelvin(temperature_c):
+    return temperature_c + 273.15
+
+
+def _kelvin_to_celcius(temperature_k):
+    return temperature_k - 273.15
+
+
+# PR103J2 thermistor datasheet:
+# https://drive.google.com/open?id=1XYhgVEmt2UQYlyh7GxlQNlW7ZMkUmiaH
+T_0_CELCIUS = 25
+T_0_KELVIN = _celcius_to_kelvin(T_0_CELCIUS)
+R0_PR103J2_DATASHEET = 10000  # Resistance at T0
+BETA_PR103J2_DATASHEET = 3892
+
+
+def thermistor_resistance_given_temperature(temperature_c, beta=BETA_PR103J2_DATASHEET, r0=R0_PR103J2_DATASHEET):
+    '''Predict thermistor resistance given temperature using the "Beta parameter equation" from
+        https://en.wikipedia.org/wiki/Thermistor
+
+    Args:
+        temperature_c:
+        beta: Optional (defaults to BETA_PR103J2_DATASHEET). Thermistor "Beta parameter"
+        r0: Optional (defaults to R0_PR103J2_DATASHEET). Thermistor resistance at T0
+    '''
+    Rinf = r0 * np.exp(-beta / T_0_KELVIN)
+
+    temperature_k = _celcius_to_kelvin(temperature_c)
+
+    return Rinf * np.exp(beta / temperature_k)
+
+
+def voltage_divider_vout_given_resistances(r2, r1, v_in=RASPBERRY_PI_VOLTAGE_3_3):
+    ''' Calculate v_out given voltage divider resistors, r1 and r2, and v_in, where the divider is set up:
+        |---v_in
+        |
+        r1
+        |
+        |---v_out
+        |
+        r2
+        |
+        |---Ground
+
+    Args:
+        r2: Resistance (in ohms) of r2 in a voltage divider
+        r1: Resistance (in ohms) of r1 in a voltage divider
+        v_in: Optional (defaults to RASPBERRY_PI_VOLTAGE_3_3). The input voltage to the voltage divider.
+
+    Returns:
+        v_out: Output voltage of the voltage divider
+    '''
+    return v_in * (r2 / (r1 + r2))
+
+
+def digital_count_given_voltage(analog_in, v_max=ADS1115_MAX_VOLTAGE_AT_GAIN_1, bit_depth=ADS1115_BIT_DEPTH):
+    ''' Calculate the digital output of an ADC given the analog_in and the reference v_max
+
+    Args:
+        analog_in: Input analog voltage coming into the ADC
+        v_max: Optional (defaults to ADS1115_MAX_VOLTAGE_AT_GAIN_1). Reference max voltage of the ADC
+        bit_depth: Optional (defaults ADS1115_BIT_DEPTH). The bit depth of the ADC.
+    '''
+    max_count = 2 ** (bit_depth - 1) - 1  # The ADC returns a signed int, so use (bit_depth - 1)
+
+    # Intentionally don't round to an int here, to make simulated math more consistent later
+    return (analog_in / v_max) * max_count
+
+
+def digital_count_given_temperature(
+    temperature_c,
+    voltage_divider_resistor,
+    thermistor_beta=BETA_PR103J2_DATASHEET,
+    thermistor_r0=R0_PR103J2_DATASHEET,
+    voltage_divider_v_in=RASPBERRY_PI_VOLTAGE_3_3,
+    adc_v_max=ADS1115_MAX_VOLTAGE_AT_GAIN_1,
+    adc_bit_depth=ADS1115_BIT_DEPTH
+):
+    ''' Calculate the digital output of an ADC given temperature, using a thermistor in a voltage divider:
+
+        |---v_in
+        |
+        r1=voltage_divider_resistor
+        |
+        |---v_out --> analog input into ADC
+        |
+        r2=thermistor
+        |
+        |---Ground
+
+        This function is expected to be used to simulate what digital counts we might expect at given temperatures
+
+    Args:
+        temperature_c: Temperature in Celcius
+        voltage_divider_resistor: Resistance of the r1 resistor in the voltage divider
+        thermistor_beta: Optional (defaults to BETA_PR103J2_DATASHEET). Thermistor "Beta parameter".
+        thermistor_r0: Optional (defaults to R0_PR103J2_DATASHEET). Thermistor resistance at T0.
+        voltage_divider_v_in: Optional (defaults to RASPBERRY_PI_VOLTAGE_3_3). The input voltage to the voltage divider.
+        adc_v_max: Optional (defaults to ADS1115_MAX_VOLTAGE_AT_GAIN_1). Reference max voltage of the ADC.
+        adc_bit_depth: Optional (defaults to ADS1115_BIT_DEPTH). The bit depth of the ADC.
+
+    Returns:
+        digital_count: a signed integer (matching adc_bit_depth) that represents a digital temperature measurement
+    '''
+    thermistor = thermistor_resistance_given_temperature(temperature_c, thermistor_beta, thermistor_r0)
+    v_out = voltage_divider_vout_given_resistances(
+        r2=thermistor,
+        r1=voltage_divider_resistor,
+        v_in=voltage_divider_v_in
+    )
+    digital_count = digital_count_given_voltage(analog_in=v_out, v_max=adc_v_max, bit_depth=adc_bit_depth)
+    return digital_count
+
+
+def temperature_given_digital_count(
+    digital_count,
+    voltage_divider_resistor,
+    thermistor_beta=BETA_PR103J2_DATASHEET,
+    thermistor_r0=R0_PR103J2_DATASHEET,
+    voltage_divider_v_in=RASPBERRY_PI_VOLTAGE_3_3,
+    adc_v_max=ADS1115_MAX_VOLTAGE_AT_GAIN_1,
+    adc_bit_depth=ADS1115_BIT_DEPTH
+):
+    ''' Calculate the temperature given a measured digital count from an ADC, using a thermistor in a voltage divider:
+
+        |---v_in
+        |
+        r1=voltage_divider_resistor
+        |
+        |---v_out --> analog input into ADC
+        |
+        r2=thermistor
+        |
+        |---Ground
+
+        This function is expected to be used to generate a specific curve_fit function
+        (i.e. with hardcoded constants) to match calibration data.
+
+    Args:
+        digital_count: a signed integer (matching adc_bit_depth) that represents a digital temperature measurement
+        voltage_divider_resistor: Resistance of the r1 resistor in the voltage divider
+        thermistor_beta: Optional (defaults to BETA_PR103J2_DATASHEET). Thermistor "Beta parameter".
+        thermistor_r0: Optional (defaults to R0_PR103J2_DATASHEET). Thermistor resistance at T0.
+        voltage_divider_v_in: Optional (defaults to RASPBERRY_PI_VOLTAGE_3_3). The input voltage to the voltage divider.
+        adc_v_max: Optional (defaults to ADS1115_MAX_VOLTAGE_AT_GAIN_1). Reference max voltage of the ADC.
+        adc_bit_depth: Optional (defaults to ADS1115_BIT_DEPTH). The bit depth of the ADC.
+
+    Returns:
+        temperature: Calculated temperature in Celcius
+    '''
+    # This function was artisenally reversed from the digital_count_given_temperature function using pencil and paper
+    numerator = (voltage_divider_resistor) / ((thermistor_r0 * np.exp(-thermistor_beta / T_0_KELVIN)))
+    denominator = ((2 ** (adc_bit_depth - 1) - 1) * voltage_divider_v_in) / (digital_count * adc_v_max) - 1
+    temperature_k = thermistor_beta / np.log(numerator / denominator)
+    temperature_c = _kelvin_to_celcius(temperature_k)
+    return temperature_c
+
+
+# These constants were calculated using scipy's curve_fit in the jupyter notebook:
+# '2019-04-30 Temperature Calibration.ipynb'
+VOLTAGE_DIVIDER_RESISTOR_CALIBRATED = 5001.89789
+BETA_CALIBRATED = 3906.51696
+VOLTAGE_DIVIDER_VIN_CALIBRATED = 3.29490430
+
+
+def temperature_given_digital_count_calibrated(digital_count):
+    return temperature_given_digital_count(
+        digital_count,
+        voltage_divider_resistor=VOLTAGE_DIVIDER_RESISTOR_CALIBRATED,
+        thermistor_beta=BETA_CALIBRATED,
+        thermistor_r0=R0_PR103J2_DATASHEET,
+        voltage_divider_v_in=VOLTAGE_DIVIDER_VIN_CALIBRATED,
+        adc_v_max=ADS1115_MAX_VOLTAGE_AT_GAIN_1,
+        adc_bit_depth=ADS1115_BIT_DEPTH
+    )
---
+++ osmo_camera/calibration/temperature_test.py
@@ -0,0 +1,94 @@
+import pkg_resources
+
+import numpy as np
+import pandas as pd
+import pytest
+
+from . import temperature as module
+
+
+PR103J2_temperature_vs_resistance_path = pkg_resources.resource_filename(
+    'osmo_camera',
+    'calibration/PR103J2_temperature_vs_resistance.csv'
+)
+
+
+class TestThermistorResistanceGivenTemperature:
+    def test_returns_R0_at_T0(self):
+        actual = module.thermistor_resistance_given_temperature(temperature_c=module.T_0_CELCIUS)
+
+        assert actual == module.R0_PR103J2_DATASHEET
+
+    def test_roughly_matches_datasheet_table(self):
+        datasheet_table = pd.read_csv(PR103J2_temperature_vs_resistance_path)
+
+        calculated_resistances = datasheet_table['temperature'].apply(module.thermistor_resistance_given_temperature)
+        expected_resistances = datasheet_table['resistance']
+
+        np.testing.assert_allclose(expected_resistances, calculated_resistances, rtol=0.02)
+
+
+class TestVoltageDividerVoutGivenResistances:
+    @pytest.mark.parametrize('name, r2, r1, v_in, expected_v_out', [
+        ('divides in half', 10, 10, 10, 5),
+        ('divides in quarter', 10, 30, 10, 2.5),
+    ])
+    def test_voltage_divider_vout_given_resistances(self, name, r2, r1, v_in, expected_v_out):
+        actual_Vout = module.voltage_divider_vout_given_resistances(r2, r1, v_in)
+        assert actual_Vout == expected_v_out
+
+    def test_defaults_to_raspberry_pi_voltage(self):
+        actual_Vout = module.voltage_divider_vout_given_resistances(r2=1, r1=1)
+        assert actual_Vout == module.RASPBERRY_PI_VOLTAGE_3_3 / 2
+
+
+class TestDigitalCountGivenVoltage:
+    @pytest.mark.parametrize('analog_in, v_max, expected_digital_count', [
+        (-5, 10, -16383.5),
+        (5, 10, 16383.5),
+        (10, 10, 32767),
+    ])
+    def test_spot_check_digital_count_given_voltage(self, analog_in, v_max, expected_digital_count):
+        actual = module.digital_count_given_voltage(analog_in, v_max, bit_depth=16)
+        assert pytest.approx(actual) == expected_digital_count
+
+    def test_defaults_to_adc_vmax(self):
+        expected_digital_count = 2**15 - 1
+        actual = module.digital_count_given_voltage(analog_in=4.096)
+        assert pytest.approx(actual) == expected_digital_count
+
+
+# Share spot-check test cases between digital_count_given_temperature and temperature_given_digital_count
+SPOT_CHECK_TEST_CASES = [
+    # temperature, resistor, digital_count
+    (25, 10000, 13199.597167),
+    (0, 10000, 20263.742126),
+    (25, 5000, 17599.462890),
+    (0, 5000, 22928.109841),
+]
+
+
+class TestDigitalCountAndTemperatureFunctions:
+    @pytest.mark.parametrize('temperature, resistor, expected_digital_count', SPOT_CHECK_TEST_CASES)
+    def test_spot_check_digital_count_given_temperature(self, temperature, resistor, expected_digital_count):
+        actual = module.digital_count_given_temperature(temperature, voltage_divider_resistor=resistor)
+        assert pytest.approx(actual) == expected_digital_count
+
+    @pytest.mark.parametrize('expected_temperature, resistor, digital_count', SPOT_CHECK_TEST_CASES)
+    def test_spot_check_temperature_given_digital_count(self, expected_temperature, resistor, digital_count):
+        actual = module.temperature_given_digital_count(
+            digital_count,
+            voltage_divider_resistor=resistor,
+        )
+        assert pytest.approx(actual, abs=0.01) == expected_temperature
+
+    def test_digital_count_given_temperature_round_trip(self):
+        resistor = 10000
+        temperatures = pd.Series([0, 7.5, 15, 25, 30, 35])
+        digital_counts = module.digital_count_given_temperature(temperatures, voltage_divider_resistor=resistor)
+        round_trip_temperatures = module.temperature_given_digital_count(
+            digital_counts,
+            voltage_divider_resistor=resistor,
+        )
+
+        pd.testing.assert_series_equal(temperatures, round_trip_temperatures)
